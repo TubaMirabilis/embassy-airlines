6f19cd3e0d354b8cc608bbd3e28e6cad2507b7f8
Ben Green <bkingsleygreen@gmail.com>
2023-06-09 10:56:24 +0100

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [autoprefixer],
+};
---
+++ src/root.tsx
@@ -0,0 +1,44 @@
+import { Suspense } from "solid-js";
+import {
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start";
+import Navbar from "./components/Navbar";
+import "./root.css";
+
+export default function Root() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Title>Embassy Airlines</Title>
+        <Meta charset="utf-8" />
+        <Meta name="description" content="Canada's Friendly Airline" />
+        <Meta
+          name="keywords"
+          content="Canadian Airline, Vancouver Airline, Vancouver flights"
+        />
+        <Meta name="author" content="Embassy Airlines" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Navbar />
+        <Suspense>
+          <ErrorBoundary>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
