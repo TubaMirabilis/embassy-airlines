53bbb959c7645359cbcd4506d836cae0520159c1
Ben Green <bkingsleygreen@gmail.com>
2023-06-10 05:06:11 +0100

    Interesting Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,63 @@
+import { createEffect, createSignal } from "solid-js";
+import "../styles/animations.css";
 import "../styles/navbar.css";
+import "../styles/utilities.css";
 export default function Navbar() {
-  <header class="header">
-    <div class="overlay has-fade"></div>
-    <nav class="container container--pall flex flex-jc-sb flex-ai-c">
-      <a href="/" class="header__logo">
-        <img src="images/logo.svg" alt="Easybank" />
-      </a>
-      <a id="btnHamburger" href="#" class="header__toggle hide-for-desktop">
-        <span></span>
-        <span></span>
-        <span></span>
-      </a>
-      <div class="header__links hide-for-mobile">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-        <a href="#">Blog</a>
-        <a href="#">Careers</a>
+  const [headerOpen, setHeaderOpen] = createSignal(false);
+
+  createEffect(() => {
+    if (typeof window !== "undefined") {
+      const fadeElems = document.querySelectorAll(".has-fade");
+      const toggleFade = () => {
+        if (headerOpen()) {
+          document.body.classList.add("noscroll");
+          fadeElems.forEach(function (element) {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+          });
+        } else {
+          document.body.classList.remove("noscroll");
+          fadeElems.forEach(function (element) {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+          });
+        }
+      };
+
+      toggleFade();
+    }
+  });
+  return (
+    <header class={`header ${headerOpen() ? "open" : ""}`}>
+      <div class="overlay has-fade"></div>
+      <nav class="d-flex justify-content-between justify-content-lg-start ai-center">
+        <a href="/" class="header-logo">
+          <img src="brand.png" alt="Embassy" />
+        </a>
+        <a
+          href="#"
+          class="header-toggle d-lg-none"
+          onclick={() => setHeaderOpen(!headerOpen())}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </a>
+        <div class="header-links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Blog</a>
+          <a href="#">Careers</a>
+        </div>
+      </nav>
+      <div class="header-menu has-fade">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Blog</a>
+        <a href="">Careers</a>
       </div>
-      <a href="#" class="button header__cta hide-for-mobile">
-        Request Invite
-      </a>
-    </nav>
-    <div class="header__menu has-fade">
-      <a href="">Home</a>
-      <a href="">About</a>
-      <a href="">Contact</a>
-      <a href="">Blog</a>
-      <a href="">Careers</a>
-    </div>
-  </header>;
+    </header>
+  );
 }
